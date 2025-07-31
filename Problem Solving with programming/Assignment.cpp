669c93ee4414263f6f09d020c9e5cb2273de3e66
swl16 <sweili165@gmail.com>
2025-07-31 23:56:47 +0800

    welcome page

--- Problem Solving with programming/Assignment.cpp
+++ Problem Solving with programming/Assignment.cpp
@@ -12,12 +12,15 @@ int main()
 	cout << "   -----------  \n";
 	cout << "    ^^    ^^    \n";
 
+	int num;
 
+	cout << "Welcome to TWD Train ticket system!\n";
+	cout << "Please select\n";
+	cout << "1. Buy ticket\n";
+	cout << "2. Contact us\n";
+	cout << "3. Exit\n";
 
-
-
-
-
+	cin >> num;
 
 
 	return 0;
