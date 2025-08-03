74f6b4ee4558088e727bba5518848dc9f426c6d5
Xuan <potatwoo0214@gmail.com>
2025-08-03 13:18:06 +0800

    Update Assignment.cpp

--- Problem Solving with programming/Assignment.cpp
+++ Problem Solving with programming/Assignment.cpp
@@ -18,7 +18,7 @@ int main()
 	cout << "  /--         | \n";
 	cout << "  |           | \n";
 	cout << "   -----------  \n";
-	cout << "    ^^    ^^    \n";
+	cout << "    ooo   ooo   \n";
 
 	cout << "\nWelcome to TWD Train Ticket Booking System!\n\n";
 
