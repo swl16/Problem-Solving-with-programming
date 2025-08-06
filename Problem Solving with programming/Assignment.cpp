5d1b217c15c452aafecb557d66bbd2c7aed0dddd
swl16 <sweili165@gmail.com>
2025-08-06 13:36:51 +0800

    1

--- Problem Solving with programming/Assignment.cpp
+++ Problem Solving with programming/Assignment.cpp
@@ -63,6 +63,7 @@ int main()
 			cin >> ic_number;
 			cout << "Date of Birth (DD/MM/YYYY) : ";
 			cin >> date >> slash_1 >> month >> slash_2 >> year;
+			cin.ignore();
 			cout << "Gender (F/M) : ";
 			cin >> gender;
 
